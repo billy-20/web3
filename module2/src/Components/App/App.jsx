78d420b86848c8f5fd7702aa57dfd1ca1fbe99a0
Bilal El Haddadi <bilal.elhaddadi@student.vinci.be>
2022-09-23 11:28:23 +0200

    module 2: init

--- module2/src/Components/App/App.jsx
+++ module2/src/Components/App/App.jsx
@@ -1,33 +1,37 @@
 
-props = {
-    name: 'Bilal',
-    age: 22,
-  }
-  const Hello = ({ name , age }) => {
 
-    const bornYear = () => new Date().getFullYear() - age
-  
-    return (
-      <div>
-        <p>
-          Hello {name}, you are {age} years old
-        </p>
-        <p>So you were probably born in {bornYear()}</p>
-      </div>
-    )
-  }
-  
+  import Display from 'Components/Display/Display'
+  import Button from 'Components/Button/Button'
+
+  import { useState } from 'react'
   const App = () => {
-    const name = 'Peter'
-    const age = 10
+    const [ counter, setCounter ] = useState(0)
+  
+    const increaseByOne = () => setCounter(counter + 1)
+  
+    const setToZero = () => setCounter(0)
+
+    const decreasedByOne = () => setCounter(counter -1);
   
     return (
       <div>
-        <h1>Greetings</h1>
-        <Hello name="Maya" age={26 + 10} />
-        <Hello name={name} age={age} />
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      /> 
+       <Button
+        onClick={decreasedByOne}
+        text='minus'
+      /> 
       </div>
     )
   }
-
+  
   export default App
+  
+
--- module2/src/index.js
+++ module2/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import './index.css';
 
 import App from './Components/App/App'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
